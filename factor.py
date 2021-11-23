bb92a822624ac656ec7f8c3691c431e87b77624b
Zeehan0133 <zyne0133@gmail.com>
2021-11-23 22:50:46 +0530

    [Zeehan]Added(Prime Factor of a given Number program)

---
+++ factor.py
@@ -0,0 +1,10 @@
+"""Prime Factor of a given Number program"""
+n=int(input("Enter a number "))
+i=2
+while(n!=1):
+    rem=n%i
+    if (rem==0):
+        n=n/i
+        print(i)
+    else:
+        i=i+1
